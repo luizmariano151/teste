6155d231767a734534d8fa99bb60ed5641d6d56d
luiz mariano <luizmarianojunior151@gmail.com>
2022-06-21 15:32:49 -0300

    init

---
+++ src/test/java/com/desafio/klok/teste/automacao/AutomacaoWeb.java
@@ -0,0 +1,83 @@
+package com.desafio.klok.teste.automacao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AutomacaoWeb {
+
+	private static WebDriver driver;
+	private static Long TIMEOUT_IN_SECONDS = 2L;
+	
+	@BeforeAll
+	public static void setup() throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver", "C:\\drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		new WebDriverWait(driver, TIMEOUT_IN_SECONDS);
+		
+		driver.get("https://www.magazineluiza.com.br/");
+		Thread.sleep(2000);
+	}
+	
+	@AfterAll
+	public static void finish() {
+		driver.quit();
+	}
+	
+	@Test
+	public void testSearchProductSuccess1() throws InterruptedException {
+		WebElement inputSearch = driver.findElement(By.id("input-search")); 
+		inputSearch.click();
+		String productName = "SMARTPHONE 9T Dual 128GB - CINZA - Global";
+		inputSearch.sendKeys(productName);
+		Thread.sleep(5000);
+		
+		inputSearch.sendKeys(Keys.ENTER);
+		Thread.sleep(10000);
+		
+		WebElement product = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/section[4]/div[3]/div/ul/li/a/div[3]/h2"));
+		assertEquals(productName, product.getText());
+		Thread.sleep(2000);
+	}
+	
+	@Test
+	public void testSearchProductFail2() throws InterruptedException {
+		WebElement inputSearch = driver.findElement(By.id("input-search"));
+		inputSearch.click();
+		String productName = "SMARTPHONE 9T Dual 128GB";
+		
+		for(int i = 0;i < 41;i++) {
+			inputSearch.sendKeys(Keys.BACK_SPACE);
+		}
+		
+		for(int i = 0;i < 3;i++) {
+			inputSearch.sendKeys(Keys.DELETE);
+		}
+		
+		inputSearch.sendKeys(productName);
+		Thread.sleep(5000);
+		
+		inputSearch.sendKeys(Keys.ENTER);
+		Thread.sleep(10000);
+	
+		WebElement product = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/section[4]/div[3]/div/ul/li[1]/a/div[3]/h2"));
+		assertNotEquals(productName, product.getText());
+		Thread.sleep(2000);
+	}
+	
+}
